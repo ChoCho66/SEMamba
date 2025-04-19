6683a10acf6162015c3aca5cce06503ac05db40c
ChoCho66 <porkdie@gmail.com>
2025-04-19 16:43:36 +0800

    筆電

---
+++ run.sh
@@ -0,0 +1,6 @@
+CUDA_VISIBLE_DEVICES='0' python train.py \
+  --config recipes/SEMamba_advanced/SEMamba_advanced.yaml \
+  --exp_folder exp20250319 \
+  --exp_name SEMamba_v1 \
+
+# Generator Parameters : 2258769
